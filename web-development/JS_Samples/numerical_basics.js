a88a35cf7532bc1bebd0e83a1ab1b7f506fbcbb0
masreed <bluekitdrummer@gmail.com>
2021-02-02 10:56:28 -0600

    Initial Commit

---
+++ web-development/JS_Samples/numerical_basics.js
@@ -0,0 +1,7 @@
+//Calculate the age of a dog in human years.
+var dogAge = prompt("Enter the age of your dog in years.");
+
+//Exemplifies numerical precedence
+var humanAge = (dogAge - 2) * 4 + 21;
+
+alert("Your dog's human age is : " + humanAge);
