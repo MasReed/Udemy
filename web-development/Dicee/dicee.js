ab6402585a79ddba5a7a3a7c442c1bed3fc520cf
masreed <bluekitdrummer@gmail.com>
2021-02-03 13:11:06 -0600

    Base Functionality

--- web-development/Dicee/dicee.js
+++ web-development/Dicee/dicee.js
@@ -1,6 +1,18 @@
-
+//Generate random number 1-6 for each player
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+//Change dice image to reflect random number
 document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+//Interpret winner and display message
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🎲 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🎲";
+} else {
+    document.querySelector("h1").innerHTML = "🎲 DRAW 🎲";
+}
+
+//Potential features: Add/Remove player up to 4 players.
