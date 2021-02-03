71f7cffc9f6c41e7906ef495e8e2e3511d5be16a
masreed <bluekitdrummer@gmail.com>
2021-02-02 19:01:06 -0600

    Updated with flow control

--- web-development/JS_Samples/bmi_function.js
+++ web-development/JS_Samples/bmi_function.js
@@ -1,8 +1,15 @@
-function bmiCalculator(weight, height) {
-    //Calculate bmi based on a users weight and height.
-    return Math.round(weight / (height**2.));
+function bmiCalculator (weight, height) {
+    //Calculate bmi given weight and height and interpret health
+    var bmi = Math.round(weight / (height * height));
+    var interpretation = "";
+
+    if (bmi > 24.9){
+        interpretation = " are overweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = " have a normal weight.";
+    } else {
+        interpretation = " are underweight.";
+    }
+
+    return "Your BMI is " + bmi + ", so you" + interpretation;
 }
-
-
-var bmi = bmiCalculator(65, 1.8); //answer of 20
-console.log(bmi)
