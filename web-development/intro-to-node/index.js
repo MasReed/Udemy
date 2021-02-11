1094773459afc29bf54908b45ba4e89ca2eea0bb
masreed <bluekitdrummer@gmail.com>
2021-02-11 12:08:06 -0600

    created node.js intro

---
+++ web-development/intro-to-node/index.js
@@ -0,0 +1,11 @@
+
+
+var villain = require("supervillains");
+var hero = require("superheroes");
+
+
+villainName = villain.random();
+heroName = hero.random();
+
+
+console.log(villainName + " vs. " + heroName);
