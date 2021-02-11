5f20a16a77ad35023fb90a68f9a435f7ebfcb436
masreed <bluekitdrummer@gmail.com>
2021-02-11 14:10:34 -0600

    Added BMI Calculator

--- web-development/backend/calculator-server/calculator.js
+++ web-development/backend/calculator-server/calculator.js
@@ -8,6 +8,8 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// A SIMPLE ADDITION
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -22,6 +24,23 @@ app.post('/', function(req, res){
     res.send("The addition of " + num1 + " + " + num2 + " is: " + result);
 });
 
+
+// CALCULATE BMI
+app.get('/bmiCalculator', function(req, res){
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmiCalculator', function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("The BMI is " + bmi);
+});
+
+
+// listen for server
 app.listen(port, function(){
     console.log("server started on port " + port);
 });
