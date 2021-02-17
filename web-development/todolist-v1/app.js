9e30ccbaaee92743eaaecd9a479369f933f0eb5a
masreed <bluekitdrummer@gmail.com>
2021-02-16 18:36:45 -0600

    Utilize ejs header/footer

--- web-development/todolist-v1/app.js
+++ web-development/todolist-v1/app.js
@@ -5,11 +5,14 @@ const port = 3000;
 const app = express();
 
 let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
+// Home Route
 app.get('/', function(req, res){
 
     let options = {
@@ -21,19 +24,39 @@ app.get('/', function(req, res){
 
     let date = new Date().toLocaleDateString("en-AU", options);
 
-    res.render('list', {kindOfDay: date,
-                        newItems: items,
-                       })
+    res.render('list', {listTitle: date, newItems: items})
 });
-
 
 app.post('/', function(req, res){
     item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+
+    if (req.body.list === 'Work List'){
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
 });
 
 
+// Work Route
+app.get('/work', function(req, res){
+    res.render('list', {listTitle: "Work List", newItems: workItems});
+});
+
+
+app.post('/work', function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect('/work');
+});
+
+
+// About Route
+app.get('/about', function(req, res){
+    res.render('about');
+});
 
 app.listen(port, function(){
     console.log("Server started on port " + port)
