5ff7900e89a8f50e9bc856679c6338c07a8a11a5
masreed <bluekitdrummer@gmail.com>
2021-02-24 14:26:37 -0600

    Add/Remove item functionality

--- web-development/todolist-v2/app.js
+++ web-development/todolist-v2/app.js
@@ -10,49 +10,52 @@ const app = express();
 
 // Utilize EJS, body-parser, express
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static('public'));
 
+
 // Connect to MongoDB server
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 
-// Data schema
+// Item schema
 const itemsSchema = new mongoose.Schema({
     name: String
 });
 
 const Item = new mongoose.model("Item", itemsSchema);
 
-
 // Default items
-const item1 = new Item ({
+const item1 = new Item({
     name: "Welcome to your todo list!"
 });
 
-const item2 = new Item ({
+const item2 = new Item({
     name: "Hit the '+' button to add a new item."
 });
 
-const item3 = new Item ({
+const item3 = new Item({
     name: "<-- Check this to cross off an item."
 });
 
 const defaultItems = [item1, item2, item3]
 
 
+// HOME ROUTE
+app.get('/', function(req, res) {
 
-// Home Route
-app.get('/', function(req, res){
-
-    //Get all items in DB
-    Item.find({}, function(err, allItems){
-        if (err){
+    // Get all items in DB
+    Item.find({}, function(err, allItems) {
+        if (err) {
             console.log(err);
         } else if (allItems.length === 0) {
             // Insert default items only if no other items
-            Item.insertMany(defaultItems, function(err){
+            Item.insertMany(defaultItems, function(err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -61,19 +64,23 @@ app.get('/', function(req, res){
             });
             res.redirect('/');
         } else {
-            res.render('list', {listTitle: date.getDate(), newItems: allItems})
+            res.render('list', {
+                listTitle: date.getDate(),
+                newItems: allItems
+            })
         }
     });
-
 });
 
-app.post('/', function(req, res){
+// Add New Item
+app.post('/', function(req, res) {
+
     const itemName = req.body.newItem;
     const item = new Item({
         name: itemName
     });
 
-    if (req.body.list === 'Work List'){
+    if (req.body.list === 'Work List') {
         workItems.push(item);
         res.redirect('/work');
     } else {
@@ -82,25 +89,45 @@ app.post('/', function(req, res){
     }
 });
 
+// Delete Item
+app.post('/delete', function(req, res) {
 
-// Work Route
-app.get('/work', function(req, res){
-    res.render('list', {listTitle: "Work List", newItems: workItems});
+    const checkedItemId = req.body.checkbox;
+
+    Item.findByIdAndRemove(checkedItemId, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 
-app.post('/work', function(req, res){
+// WORK ROUTE
+app.get('/work', function(req, res) {
+    res.render('list', {
+        listTitle: "Work List",
+        newItems: workItems
+    });
+});
+
+app.post('/work', function(req, res) {
+
     const item = req.body.newItem;
+
     workItems.push(item);
     res.redirect('/work');
 });
 
 
-// About Route
-app.get('/about', function(req, res){
+// ABOUT ROUTE
+app.get('/about', function(req, res) {
     res.render('about');
 });
 
-app.listen(port, function(){
+
+// Port connection
+app.listen(port, function() {
     console.log("Server started on port " + port)
 });
