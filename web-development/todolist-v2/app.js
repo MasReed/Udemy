02adb95c02827f703a5f74b80d527b5183b49d3f
masreed <bluekitdrummer@gmail.com>
2021-02-24 14:02:30 -0600

    Implement mongoDB/mongoose for todo app

--- web-development/todolist-v2/app.js
+++ web-development/todolist-v2/app.js
@@ -1,31 +1,83 @@
+// Require packages
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js'); //local module
 
+// App instance
 const port = 3000;
 const app = express();
 
-const items = ['Buy Food', 'Cook Food', 'Eat Food'];
-const workItems = [];
-
+// Utilize EJS, body-parser, express
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+// Connect to MongoDB server
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// Data schema
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = new mongoose.model("Item", itemsSchema);
+
+
+// Default items
+const item1 = new Item ({
+    name: "Welcome to your todo list!"
+});
+
+const item2 = new Item ({
+    name: "Hit the '+' button to add a new item."
+});
+
+const item3 = new Item ({
+    name: "<-- Check this to cross off an item."
+});
+
+const defaultItems = [item1, item2, item3]
+
+
+
 // Home Route
 app.get('/', function(req, res){
-    res.render('list', {listTitle: date.getDate(), newItems: items})
+
+    //Get all items in DB
+    Item.find({}, function(err, allItems){
+        if (err){
+            console.log(err);
+        } else if (allItems.length === 0) {
+            // Insert default items only if no other items
+            Item.insertMany(defaultItems, function(err){
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Saved default items to DB");
+                }
+            });
+            res.redirect('/');
+        } else {
+            res.render('list', {listTitle: date.getDate(), newItems: allItems})
+        }
+    });
+
 });
 
 app.post('/', function(req, res){
-    item = req.body.newItem;
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    });
 
     if (req.body.list === 'Work List'){
         workItems.push(item);
         res.redirect('/work');
     } else {
-        items.push(item);
+        item.save();
         res.redirect('/');
     }
 });
